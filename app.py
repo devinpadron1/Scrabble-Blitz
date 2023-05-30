e688f9c7df6109fd73a9775bfa79fd563aaa480b
Devin <devinpadron28@gmail.com>
2023-05-30 15:58:39 -0400

    Import random word from scrabble dictionary to game board.

--- app.py
+++ app.py
@@ -1,13 +1,110 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+import random
 
 app = Flask(__name__)
 
-@app.route('/play', methods=['GET', 'POST'])
+with open("scrabble-words.txt", "r") as file:
+    words = file.read().split()
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/word', methods=['GET'])
 def get_word():
     # This is where you would get a word from your word source.
-    # For the time being, let's just return a static word.
-    return jsonify({"word": "HELLO"})
+    
+    # Initialize key variables
+    points = amount = None
+
+    # Create tiles with values and quantities
+    default_tiles = {
+        'A': {points: 1, amount: 9},
+        'B': {points: 3, amount: 2},
+        'C': {points: 3, amount: 2},
+        'D': {points: 2, amount: 4},
+        'E': {points: 1, amount: 12},
+        'F': {points: 4, amount: 2},
+        'G': {points: 2, amount: 3},
+        'H': {points: 4, amount: 2},
+        'I': {points: 1, amount: 9},
+        'J': {points: 8, amount: 1},
+        'K': {points: 5, amount: 1},
+        'L': {points: 1, amount: 4},
+        'M': {points: 3, amount: 2},
+        'N': {points: 1, amount: 6},
+        'O': {points: 1, amount: 8},
+        'P': {points: 3, amount: 2},
+        'Q': {points: 10, amount: 1},
+        'R': {points: 1, amount: 6},
+        'S': {points: 1, amount: 4},
+        'T': {points: 1, amount: 6},
+        'U': {points: 1, amount: 4},
+        'V': {points: 4, amount: 2},
+        'W': {points: 4, amount: 2},
+        'X': {points: 8, amount: 1},
+        'Y': {points: 4, amount: 2},
+        'Z': {points: 10, amount: 1},
+        '_': {points: 0, amount: 2}, # blank tiles
+      }
+    current_tiles = {
+        'A': {'amount': 9},
+        'B': {'amount': 2},
+        'C': {'amount': 2},
+        'D': {'amount': 4},
+        'E': {'amount': 12},
+        'F': {'amount': 2},
+        'G': {'amount': 3},
+        'H': {'amount': 2},
+        'I': {'amount': 9},
+        'J': {'amount': 1},
+        'K': {'amount': 1},
+        'L': {'amount': 4},
+        'M': {'amount': 2},
+        'N': {'amount': 6},
+        'O': {'amount': 8},
+        'P': {'amount': 2},
+        'Q': {'amount': 1},
+        'R': {'amount': 6},
+        'S': {'amount': 4},
+        'T': {'amount': 6},
+        'U': {'amount': 4},
+        'V': {'amount': 2},
+        'W': {'amount': 2},
+        'X': {'amount': 1},
+        'Y': {'amount': 2},
+        'Z': {'amount': 1},
+        '_': {'amount': 2},  # blank tiles
+    }
+
+    while True:
+        # return a random word from scrabble dictionary
+        word = random.choice(words)
+
+        # split word into array of letters
+        word_letters = list(word)
+
+        counter = 0 # tiles removed
+        enough_tiles = True
+        for letter in word_letters:
+            if current_tiles[letter]['amount'] > 0:
+                current_tiles[letter]['amount'] -= 1
+                counter += 1
+            else: # if not enough tiles
+                # add previous letter tiles back
+                for letter in word_letters[:counter]:
+                    current_tiles[letter]['amount'] += 1
+                enough_tiles = False            
+                break # break inner loop if not enough tiles
+        
+        if enough_tiles:
+            counter = 0
+            break # break outer loop
+
+    return jsonify({"word": word})
 
 # Only run code when imported as script, not a module
 if __name__ == '__main__':
     app.run(debug=True)
+
+# TODO: The initial word can't be longer than 7 letters
---
+++ static/script.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let fetchedWord;
+
+    // Fetch word from Flask
+    fetch('http://127.0.0.1:5000/word')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        fetchedWord = data.word;
+        console.log('Type of fetchedWord:', typeof fetchedWord);
+        loadWord(fetchedWord);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+    
+    function loadWord(word) {
+        let letters = word.split('')
+        for(let i=0; i<letters.length; i++){
+            // Get a reference to the square where the letter should go
+            // You may need to adjust the calculation for the square id depending on how your board is laid out
+            let square = document.getElementById(`grid8_${0+(i)}`);
+            
+            if (square) {
+                // Place the letter in the square
+                square.innerHTML = letters[i];
+            } else {
+                console.log(`Element with id grid8_${0+(i)} does not exist`)
+            }
+
+        }
+    }
+    
+    // 2. Load the board.
+    for (let i = 1; i <= 15; i++) {
+        for (let j = 1; j <= 15; j++) {
+            // Create element
+            var boardSquare = document.createElement("div");
+            // assign its id
+            boardSquare.setAttribute("class", "board-square");
+            // assign coordinates
+            boardSquare.setAttribute("id", `grid${i}_${j}`);
+            
+            // Create sets for each special square
+            const doubleLetter = new Set(["1_4", "1_12", "3_7", "3_9", "4_1", "4_8", "4_15", "7_3", "7_7", "7_9", "7_13", "8_4", "8_12", "9_3", "9_7", "9_9", "9_13", "12_1", "12_8", "12_15", "13_7", "13_9", "15_4", "15_12"]);
+            const tripleLetter = new Set(["2_6", "2_10", "6_2", "6_6", "6_10", "6_14", "10_2", "10_6", "10_10", "10_14", "14_6", "14_10"]);
+            const doubleWord = new Set(["2_2", "2_14", "3_3", "3_13", "4_4", "4_12", "5_5", "5_11", "7_3", "7_7", "7_9", "7_13", "9_3", "9_7", "9_9", "9_13", "11_5", "11_11", "12_4", "12_12", "13_3", "13_13", "14_2", "14_14"]);
+            const tripleWord = new Set(["1_1", "1_8", "1_15", "8_1", "8_15", "15_1", "15_8", "15_15"]);
+
+            let position = `${i}_${j}`;
+
+            if (doubleLetter.has(position)) {
+                boardSquare.classList.add("double-letter");
+            } else if (tripleLetter.has(position)) {
+                boardSquare.classList.add("triple-letter");
+            } else if (doubleWord.has(position)) {
+                boardSquare.classList.add("double-word");
+            } else if (tripleWord.has(position)) {
+                boardSquare.classList.add("triple-word");
+            }
+
+            // Append to container element
+            var boardContainer = document.getElementById("board-square-container");
+            boardContainer.appendChild(boardSquare);
+        }
+    }
+
+    document.getElementById('play-button').addEventListener('click', function() {
+        document.getElementById('instructions').style.display = 'none';
+        document.getElementById('high-score-button').style.display = 'none';
+        document.getElementById('game-screen').style.display = 'flex';
+        startGame(); 
+    });
+    
+    function startGame() {
+        // Start the game: 
+        // 1. Start the timer.
+        // 3. Load the tray.
+    }
+})
+
+// TODO: Create letter tile
+// TODO: Add star to center square
+// TODO: Add letters to bonus squares
+// TODO: Load tray of letters
